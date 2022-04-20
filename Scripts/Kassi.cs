1e1bf657e6223d5bb3570bd9d9432372b024f3a7
Einar Árni Bjarnson <54355067+einsiful@users.noreply.github.com>
2022-04-20 23:13:35 +0000

    Add files via upload

---
+++ Scripts/Kassi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Kassi : MonoBehaviour
+{
+  
+    private void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(0);
+
+        }
+    }
+    
+}
